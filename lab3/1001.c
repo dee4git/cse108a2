0a7c8b239504d1c79fb5a04746be0d84797d67be
Durjoy <57084424+dee4git@users.noreply.github.com>
2023-08-01 16:21:34 +0600

    Add files via upload

---
+++ lab3/1001.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Declare variables A, B, and X
+    int A, B, X;
+
+    // Read variables A and B
+
+    scanf("%d%d",&A, &B);
+
+    // Calculate the sum
+    X = A + B;
+
+    // Print the result with an endline
+    printf("X = %d\n", X);
+
+    return 0;
+}
+
+
---
+++ lab3/1005.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main ()
+{
+    float A,B, MEDIA;
+    scanf("%f%f", &A, &B);
+    MEDIA = (A+B)/2;
+    printf("MEDIA = %.5f\n", MEDIA);
+}
